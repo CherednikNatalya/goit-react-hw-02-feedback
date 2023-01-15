86a52ea8048c6b7c5d87329d604e6fa9657c20ab
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-01-15 21:30:58 +0200

    4589

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,66 @@
-import {FeedbackOptions} from './FeedbackOptions/FeedbackOptions'
-import {Statistics} from './Statistics/Statistics'
 import React, { Component } from 'react';
-import feedbackName from '../feedback.json'
+
+import {FeedbackOptions} from './FeedbackOptions/FeedbackOptions'
+
+// import {Statistics} from './Statistics/Statistics'
+
+import {Section} from './Section/Section'
 
 
-export class Feedback extends Component {
-  state = {
-    initialFeedback: feedbackName,
-    }
-
-
-render() {
+const buttons = [
+	{ id: "good", title: "Good" },
+	{ id: "neutral", title: "Neutral" },
+	{ id: "bad", title: "Bad" }
+];
+  
+  
+export class App extends Component {
  
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+  
+  handlClickFeedback = e => {
+    const name = e.target
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  }; 
+
+  countTotalFeedback = () => {
+    const {good, neutral, bad} = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback ()
+    ? Math.round((this.state.good/this.countTotalFeedback) *100) : 0;
+  }
+
+ 
+render() {
 
   return (
-    <Feedback/>
-    <Statistics title="Statistics"good={} neutral={} bad={} total={} positivePercentage={}></Statistics>
-    <FeedbackOptions options={} onLeaveFeedback={} 
-  )
+    <>
+    <Section
+    title={"Please leave feedback"}>
+   <FeedbackOptions
+     options={buttons} 
+     onLeaveFeedback={buttons.title}/>
+    </Section>
+ 
+ {/* <Section title={"Statistics"} >
+     <Statistics 
+     good={this.good} 
+     neutral={this.neutral} 
+     bad={this.bad} 
+     total={this.countTotalFeedback} 
+     positivePercentage={this.countPositiveFeedbackPercentage}/>
+    </Section> */}
+    </>
+    
+  ) 
 }
-}
+  }
 
-
-
-
-// <h2>Statistics</h2>
-// <ul>
-//   <li>Good: {} </li>
-// </ul>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,32 +1,32 @@
-import PropTypes from 'prop-types';
-import css from './Statistics.module.css';
+// import PropTypes from 'prop-types';
+// import css from './Statistics.module.css';
 
-export const Statistics = ({title, stats}) => {
-    return (
-    <section className={css.statistics}>
-    {title && <h2 className={css.title}>{title}</h2>}
+// export const Statistics = ({title, stats}) => {
+//     return (
+//     <section className={css.statistics}>
+//     {title && <h2 className={css.title}>{title}</h2>}
   
-    <ul className={css.stat_list}>
-        {stats.map(event => (
-            <li key = {event.id} className={css.item}>
-        <span className={css.label}>{event.label}</span>
-        <span className={css.percentage}>{event.percentage}%</span>
-      </li>
-        ))} 
+//     <ul className={css.stat_list}>
+//         {stats.map(event => (
+//             <li key = {event.id} className={css.item}>
+//         <span className={css.label}>{event.label}</span>
+//         <span className={css.percentage}>{event.percentage}%</span>
+//       </li>
+//         ))} 
     
-    </ul>
-  </section>)
-}
+//     </ul>
+//   </section>)
+// }
 
-Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
-      PropTypes.exact({
-        id: PropTypes.number.isRequired,
-        label: PropTypes.string.isRequired,
-        percentage: PropTypes.number.isRequired,
-      }),
-    ),
-  };
+// Statistics.propTypes = {
+//     title: PropTypes.string,
+//     stats: PropTypes.arrayOf(
+//       PropTypes.exact({
+//         id: PropTypes.number.isRequired,
+//         label: PropTypes.string.isRequired,
+//         percentage: PropTypes.number.isRequired,
+//       }),
+//     ),
+//   };
 
-  <Statistics title="Upload stats" stats={data} />
+//   <Statistics title="Upload stats" stats={data} />
